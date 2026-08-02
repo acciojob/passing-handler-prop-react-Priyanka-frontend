db5d7105ddc6e37b880ff50e340f476bf182d084
Priyanka Kaushik <kaushikpriyanka005@gmail.com>
2026-08-02 11:28:48 +0530

    project completed

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,23 @@
+import React, { useState } from "react";
+import '../styles/Child.css';
 
+function Selection({ applyColor }) {
+
+    const [selectionStyle, setSelectionStyle] = useState({
+    background: "",
+  });
+
+  return (
+    <>
+      <div
+        className="fix-box"
+        style={selectionStyle}
+        onClick={() => applyColor(setSelectionStyle)}
+      >
+         <h3 className="subheading">Selection</h3>
+      </div>
+    </>
+  );
+}
+
+export default Selection;
